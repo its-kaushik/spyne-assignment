169f5bc6b1e03233034b92f11627c16d8a0acae0
itskaushik <mkaushik0033@gmail.com>
2024-06-30 12:40:22 +0530

    Add: Added user CRUDs

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -1,9 +1,26 @@
 import { Router } from 'express';
 import UserController from './user.controller';
+import { Validate } from '../../middlewares/validations.middleware';
+import { createUserValidation } from './user.validation';
+import { Serialize } from '../../middlewares/serializer.middleware';
+import { createUserSerializer } from './user.serializer';
 
 const UserRouter = Router();
 const Controller = new UserController();
 
-UserRouter.post('/', Controller.create);
+UserRouter.post(
+  '/',
+  Validate({ b: createUserValidation }),
+  Serialize(createUserSerializer),
+  Controller.create
+);
+
+UserRouter.get('/', Controller.find);
+
+UserRouter.patch('/', Controller.update);
+
+UserRouter.get('/:id', Controller.findOne);
+
+UserRouter.delete('/:id', Controller.delete);
 
 export default UserRouter;
