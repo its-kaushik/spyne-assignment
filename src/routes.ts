10421a95d85bfcc1aef87093eb908b5694cb01f4
itskaushik <mkaushik0033@gmail.com>
2024-06-30 12:03:24 +0530

    ADD: Added user router

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router, Request, Response } from 'express';
+import UserRouter from './modules/user/user.router';
 
 const router = Router();
 
 router.get('/health', async (req: Request, res: Response) => {
   res.json({ health: 'ok' });
 });
+
+router.use('/user', UserRouter);
 
 export default router;
